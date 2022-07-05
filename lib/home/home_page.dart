9a921c860b75889b89b593aba3c0058bd323e92c
kiengongoc <tt.kien.ngo@tomosia.com>
2022-07-05 11:32:50 +0700

    kien update

---
+++ lib/home/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Login"),
+            TextField(decoration: InputDecoration(hintText: 'KienNgo', label: Text('Username'), border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))), ),
+          ],
+        ),
+      )
+    );
+  }
+}
